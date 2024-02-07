c8922652d8001fd3b8dd396ad02a183214430ae6
Laurenz Ruzicka <Laurenz.Ruzicka@ait.ac.at>
2024-02-07 17:39:11 +0100

    .

--- whisperx/alignment.py
+++ whisperx/alignment.py
@@ -58,10 +58,10 @@ DEFAULT_ALIGN_MODELS_HF = {
 def load_align_model(language_code, device, model_name=None, model_dir=None):
     if model_name is None:
         # use default model
-        if language_code in DEFAULT_ALIGN_MODELS_TORCH:
-            model_name = DEFAULT_ALIGN_MODELS_TORCH[language_code]
-        elif language_code in DEFAULT_ALIGN_MODELS_HF:
-            model_name = DEFAULT_ALIGN_MODELS_HF[language_code]
+        if language_code.lower().strip() in DEFAULT_ALIGN_MODELS_TORCH:
+            model_name = DEFAULT_ALIGN_MODELS_TORCH[language_code.lower().strip()]
+        elif language_code.lower().strip() in DEFAULT_ALIGN_MODELS_HF:
+            model_name = DEFAULT_ALIGN_MODELS_HF[language_code.lower().strip()]
         else:
             print(f"There is no default alignment model set for this language ({language_code}).\
                 Please find a wav2vec2.0 model finetuned on this language in https://huggingface.co/models, then pass the model name in --align_model [MODEL_NAME]")
